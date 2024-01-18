0526edb7c8220d480512ae9b2b8570c48daa9156
msvt1982 <156129795+msvt1982@users.noreply.github.com>
2024-01-18 13:50:06 +0300

    fix #3

--- Create repo.sh
+++
@@ -1,9 +0,0 @@
-#echo "# Task1" >> README.md
-git init
-git add task1.txt
-git add main.go
-git commit -m "task1"
-git branch -M main
-git remote add origin https://github.com/msvt1982/Task1.git
-git push -u origin main
-
